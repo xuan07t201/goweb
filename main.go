6930e73604207f333100a1b74e5f6797f76491f2
Nguyen_Thanh_Xuan <xuan@dactech.vn>
2021-07-06 00:46:06 +0700

    ap

--- main.go
+++ main.go
@@ -51,5 +51,5 @@ func main() {
 	// Finally, we call the `ListenAndServe` with the port
 	// and a handler. `nil` tells it to use the default
 	// router we've just set up.
-	http.ListenAndServe(":8090", nil)
+	http.ListenAndServe(":5000", nil)
 }
